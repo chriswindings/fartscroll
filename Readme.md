df57b74724591a00ab5eeb8a6af921ffe7c2399f
chriswindings <email>
2022-03-24 14:57:23 -0500

    finish the js

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Getting Started
+
+Open the index.html file in chrome. The library won't play fart sounds unless the user interacts with the page first.
---
+++ index.js
@@ -0,0 +1,20 @@
+const fartSounds = [
+  new Audio('https://cdn.ramseysolutions.net/media/fs-plugin/mixkit-cartoon-fart-triple-3055.wav'),
+  new Audio('https://cdn.ramseysolutions.net/media/fs-plugin/mixkit-cartoon-quick-splat-2890.wav'),
+  new Audio('https://cdn.ramseysolutions.net/media/fs-plugin/mixkit-funny-cartoon-fast-splat-2889.wav'),
+  new Audio('https://cdn.ramseysolutions.net/media/fs-plugin/mixkit-wet-accident-fart-3041.wav'),
+]
+
+const playAudio = () => {
+  const fartIndex = getRandomFartIndex();
+  fartSounds[fartIndex].play();
+}
+
+const getRandomFartIndex = (numofFarts=4) => {
+  return Math.floor(Math.random() * numofFarts)
+}
+window.addEventListener('scroll', () => {
+  if (scrollY % 50 === 0) {
+    playAudio();
+  }
+})
