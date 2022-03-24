45d3fa5cb04b230090085bd472c3c64beaa1c129
chriswindings <email>
2022-03-24 16:28:31 -0500

    first submission

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let userHasInterracted = false;
+
 const fartSounds = [
   new Audio('https://cdn.ramseysolutions.net/media/fs-plugin/mixkit-cartoon-fart-triple-3055.wav'),
   new Audio('https://cdn.ramseysolutions.net/media/fs-plugin/mixkit-cartoon-quick-splat-2890.wav'),
@@ -6,15 +8,22 @@ const fartSounds = [
 ]
 
 const playAudio = () => {
-  const fartIndex = getRandomFartIndex();
-  fartSounds[fartIndex].play();
+  if (userHasInterracted) {
+    const fartIndex = getRandomFartIndex();
+    fartSounds[fartIndex].play();
+  }
 }
 
 const getRandomFartIndex = (numofFarts=4) => {
   return Math.floor(Math.random() * numofFarts)
 }
+
 window.addEventListener('scroll', () => {
   if (scrollY % 50 === 0) {
     playAudio();
   }
+})
+
+window.addEventListener('click', () => {
+  userHasInterracted = true;
 })
